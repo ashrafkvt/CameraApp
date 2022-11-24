2cb601ff5d9f749fdf881d02c2c54522058584ea
ashrafkvt <ashrafkvt6197@gmail.com>
2022-11-24 14:01:15 +0530

    created a VC for showing captured photo

---
+++ CameraApp-iOS/CameraApp-iOS/PhotoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoViewController.swift
+//  CameraApp-iOS
+//
+//  Created by Ashraf on 24/11/22.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+
+class PhotoViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
