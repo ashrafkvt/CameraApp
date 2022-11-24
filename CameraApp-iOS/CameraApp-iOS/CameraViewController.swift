bf2c8de7fe5e61ebfa75843af696ac07d20dc964
ashrafkvt <ashrafkvt6197@gmail.com>
2022-11-24 13:25:17 +0530

    created a VC to show camera interface

---
+++ CameraApp-iOS/CameraApp-iOS/CameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewController.swift
+//  CameraApp-iOS
+//
+//  Created by Ashraf on 24/11/22.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+
+class CameraViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
