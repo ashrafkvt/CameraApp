6d8e3700c57357ffa862f4557443ce30dc8f8336
ashrafkvt <ashrafkvt6197@gmail.com>
2022-11-25 18:55:25 +0530

    defined objects needed

--- CameraApp-iOS/CameraApp-iOS/CameraViewController.swift
+++ CameraApp-iOS/CameraApp-iOS/CameraViewController.swift
@@ -11,8 +11,36 @@ import AVFoundation
 
 
 class CameraViewController: UIViewController{
+    
+    let captureSession = AVCaptureSession()
+    var backFacingCamera: AVCaptureDevice?
+    var frontFacingCamera: AVCaptureDevice?
+    var currentDevice: AVCaptureDevice! 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
+    
+    private func configure(){
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .unspecified)
+        
+        for device in deviceDiscoverySession.devices{
+            if device.position == .back{
+                backFacingCamera = device
+            }
+            else if device.position == .front{
+                frontFacingCamera = device
+            }
+        }
+        
+        currentDevice = backFacingCamera
+        
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else{
+            return
+        }
+                
+    }
+    
 }
