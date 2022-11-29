cb69e8a72f976d6c09f376f125c1d18618767059
ashrafkvt <ashrafkvt6197@gmail.com>
2022-11-29 11:57:23 +0530

    implemented camera capturing functionality

--- CameraApp-iOS/CameraApp-iOS/CameraViewController.swift
+++ CameraApp-iOS/CameraApp-iOS/CameraViewController.swift
@@ -12,19 +12,40 @@ import AVFoundation
 
 class CameraViewController: UIViewController{
     
-    let captureSession = AVCaptureSession()
+    @IBOutlet weak var cameraButton: UIButton!
+    
     var backFacingCamera: AVCaptureDevice?
     var frontFacingCamera: AVCaptureDevice?
-    var currentDevice: AVCaptureDevice! 
+    var currentDevice: AVCaptureDevice!
+    
+    var stillImageOutput: AVCapturePhotoOutput!
+    var stillImage: UIImage?
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    let captureSession = AVCaptureSession()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .red
+        configure()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        cameraPreviewLayer?.frame = view.layer.frame
     }
     
     private func configure(){
         captureSession.sessionPreset = AVCaptureSession.Preset.photo
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .unspecified)
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInTrueDepthCamera, .builtInWideAngleCamera], mediaType: AVMediaType.video, position: .unspecified)
         
         for device in deviceDiscoverySession.devices{
             if device.position == .back{
@@ -40,7 +61,51 @@ class CameraViewController: UIViewController{
         guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else{
             return
         }
+        
+        stillImageOutput = AVCapturePhotoOutput()
+        
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(stillImageOutput)
+        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = view.layer.frame
+        
+        view.bringSubviewToFront(cameraButton)
+        captureSession.startRunning()
                 
     }
     
+    @IBAction func cameraButtonTapped(sender: UIButton){
+        let photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        photoSettings.isHighResolutionPhotoEnabled = true
+        photoSettings.flashMode = .auto
+        
+        stillImageOutput.isHighResolutionCaptureEnabled = true
+        stillImageOutput.capturePhoto(with: photoSettings, delegate: self)
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        return .landscape
+    }
+    
+    
+}
+
+
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard error == nil else {
+            return
+        }
+        
+        print("captured")
+        // Get the image from the photo buffer
+        guard let imageData = photo.fileDataRepresentation() else {
+            return
+        }
+         
+        stillImage = UIImage(data: imageData)
+    }
 }
